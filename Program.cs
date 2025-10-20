af97f8b796fb3bb6b50e6fdb0bc641774765cfc0
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2025-10-20 19:03:54 +0200

    Register the database context

---
+++ Program.cs
@@ -0,0 +1,19 @@
+
+
+
+//Register the database context: https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-9.0&tabs=visual-studio
+
+
+using Controller;
+using Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+
+app.Run(); 
